e0ca6e57176b7695371c67e1bc4f86e054fd14b9
blackreo123 <blackreo123@naver.com>
2022-05-17 09:22:36 +0900

    indicator, detailView updated

--- 8.COVID19/CovidDetailViewController.swift
+++ 8.COVID19/CovidDetailViewController.swift
@@ -17,10 +17,24 @@ class CovidDetailViewController: UITableViewController {
     @IBOutlet weak var overseasInflowCell: UITableViewCell!
     @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
     
+    var covidOverView: CovidOverView?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configureView()
         
+    }
+    
+    func configureView() {
+        guard let covidOverView = self.covidOverView else {return}
+        self.title = covidOverView.countryName
+        self.newCaseCell.detailTextLabel?.text = "\(covidOverView.newCase)명"
+        self.totalCaseCell.detailTextLabel?.text = "\(covidOverView.totalCase)명"
+        self.recoveredCell.detailTextLabel?.text = "\(covidOverView.recovered)명"
+        self.deathCell.detailTextLabel?.text = "\(covidOverView.death)명"
+        self.percentageCell.detailTextLabel?.text = "\(covidOverView.percentage)%"
+        self.overseasInflowCell.detailTextLabel?.text = "\(covidOverView.newFcase)명"
+        self.regionalOutbreakCell.detailTextLabel?.text = "\(covidOverView.newCcase)명"
     }
 }
